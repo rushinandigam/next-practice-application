ae95876e8b74ba86e037a0b8f433962b997bb22a
syamkumar12 <syamkumar.golli@sasi.ac.in>
2026-03-31 20:08:36 +0530

    hello message

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,9 @@ const Login = () => {
   return (
     <div>
       this is the login page 
+      <h1>HEllo</h1>
     </div>
   )
 }
-
+//https://github.com/rushinandigam/next-practice-application.git
 export default Login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       Home 
+      <p>Hello</p>
     </div>
   )
 }
